b58ac976b626e16e089b8f538cc9481a9425e408
david-rodriguez-levelup <72571654+david-rodriguez-levelup@users.noreply.github.com>
2020-11-25 19:37:53 +0100

    Done!

---
+++ Assets/Scripts/UI/CanvasDamageButtonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CanvasDamageButtonController : MonoBehaviour, IPointerClickHandler
+{
+    #region Fields/Properties
+
+    [SerializeField]
+    [Tooltip("LifeBehaviour affected by this damage source.")]
+    private LifeBehaviour _damageTarget;
+
+    [SerializeField]
+    [Tooltip("Level of damage (in life points) of this damage source.")]
+    private int _damageLevel = 1;
+
+    #endregion
+
+    #region IPointerClickHandler implementation
+
+    /// <summary>
+    /// Handles click inputs on the GameObject.
+    /// </summary>
+    /// <param name="pointerEventData">Event payload associated with pointer (mouse / touch) events.</param>
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        _damageTarget.ApplyDamage(_damageLevel);
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/UI/CanvasLifeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasLifeController : MonoBehaviour
+{
+
+    #region Fields/Properties
+
+    // OPTIONS 3 & 4 need this:
+    /*
+    [SerializeField]
+    [Tooltip("Player's LifeBehaviour component.")]
+    private LifeBehaviour _playerLifeBehaviour;
+    */ 
+
+    private Text _label;
+
+    private Text _value;
+
+    #endregion
+
+    #region Lifecycle
+
+    private void Awake()
+    {
+        _label = transform.Find("Label").GetComponent<Text>();
+        _label.color = Color.black;
+
+        _value = transform.Find("Value").GetComponent<Text>();
+        _value.color = Color.black;
+    }
+
+    // OPTIONS 3 & 4 need this:
+    /*
+    private void OnEnable()
+    {
+        _playerLifeBehaviour.OnLifeChangedEvent.AddListener(Refresh);
+        // DUDA!
+        //_playerLifeBehaviour.OnLifeChangedEvent.AddListener(Refresh); -- ¿Se añade otro?
+    }
+
+    private void OnDisable()
+    {
+        _playerLifeBehaviour.OnLifeChangedEvent.RemoveListener(Refresh);
+    }
+    */
+
+    #endregion
+
+    #region Methods
+
+    public void Refresh(int currentLife)
+    {
+        if (currentLife <= 0)
+        {
+            _label.color = Color.red;
+            _value.color = Color.red;
+        }
+        _value.text = Convert.ToString(currentLife);
+    }
+
+    #endregion
+
+}
